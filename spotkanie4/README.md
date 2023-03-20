c9c213316acfd3a32ccebebc76cd86e21dd39806
bpelikan <bartoszpelikan@gmail.com>
2023-03-20 22:08:00 +0100

    zadanie4.1 peerings

--- spotkanie4/README.md
+++ spotkanie4/README.md
@@ -3,3 +3,12 @@
 
 ## [Zadanie 4.1](./zadanie1)
 
+* Utworzenie sieci dla architektury Hub&Spoke.
+    * Hub vnet
+        * AzureFirewallManagementSubnet
+        * AzureFirewallSubnet
+    * Spoke vnet - Jumphost
+        * Jumphost subnet
+    * Spoke vnet - App
+        * App subnet
+* Utworzenie peeringu pomiędzy sieciami spoke <-> hub
--- spotkanie4/zadanie1/vnet.tf
+++ spotkanie4/zadanie1/vnet.tf
@@ -52,3 +52,58 @@ resource "azurerm_subnet" "app_vnet_sub01" {
   resource_group_name  = data.azurerm_resource_group.main_rg.name
   virtual_network_name = azurerm_virtual_network.app_vnet.name
 }
+
+
+
+
+resource "azurerm_virtual_network_peering" "hub_vnet_to_jumphost_vnet" {
+  name                      = "${local.hub_vnet_name}-to-${local.jumphost_vnet_name}"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.jumphost_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "jumphost_vnet_to_hub_vnet" {
+  name                      = "${local.jumphost_vnet_name}-to-${local.hub_vnet_name}"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.jumphost_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+
+
+
+resource "azurerm_virtual_network_peering" "hub_vnet_to_app_vnet" {
+  name                      = "${local.hub_vnet_name}-to-${local.app_vnet_name}"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.app_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "app_vnet_to_hub_vnet" {
+  name                      = "${local.app_vnet_name}-to-${local.hub_vnet_name}"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.app_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
