62107d07afa79bf388a3c8ec6a2cdace6ccc8442
bpelikan <bartoszpelikan@gmail.com>
2023-03-21 21:12:42 +0100

    zadanie4.2 jumphost vm

--- spotkanie4/README.md
+++ spotkanie4/README.md
@@ -12,3 +12,14 @@
     * Spoke vnet - App
         * App subnet
 * Utworzenie peeringu pomiędzy sieciami spoke <-> hub
+
+
+## [Zadanie 4.2](./zadanie2)
+
+* Publiczny adres IP dla maszyny Jumphost
+* Network interface dla maszyny wirtualnej Jumphost
+* Network Security Group dla podsieci Jumphost VM
+* Przypisanie NSG do podsieci Jumphost VM
+* VM ze skonfigurowanym logowaniem przy pomocy hasła
+* Network Security Group dla podsieci Spoke App
+* Przypisanie NSG do podsieci Spoke App
--- spotkanie4/zadanie2/vnet_hub.tf
+++ spotkanie4/zadanie2/vnet_hub.tf
@@ -22,40 +22,6 @@ resource "azurerm_subnet" "hub_vnet_sub02_fw" {
 
 
 
-resource "azurerm_virtual_network" "jumphost_vnet" {
-  name                = local.jumphost_vnet_name
-  address_space       = local.jumphost_vnet_address_space
-  resource_group_name = data.azurerm_resource_group.main_rg.name
-  location            = data.azurerm_resource_group.main_rg.location
-}
-
-resource "azurerm_subnet" "jumphost_vnet_sub01" {
-  name                 = local.jumphost_vnet_subnet01_name
-  address_prefixes     = local.jumphost_vnet_subnet01_address_space
-  resource_group_name  = data.azurerm_resource_group.main_rg.name
-  virtual_network_name = azurerm_virtual_network.jumphost_vnet.name
-}
-
-
-
-
-resource "azurerm_virtual_network" "app_vnet" {
-  name                = local.app_vnet_name
-  address_space       = local.app_vnet_address_space
-  resource_group_name = data.azurerm_resource_group.main_rg.name
-  location            = data.azurerm_resource_group.main_rg.location
-}
-
-resource "azurerm_subnet" "app_vnet_sub01" {
-  name                 = local.app_vnet_subnet01_name
-  address_prefixes     = local.app_vnet_subnet01_address_space
-  resource_group_name  = data.azurerm_resource_group.main_rg.name
-  virtual_network_name = azurerm_virtual_network.app_vnet.name
-}
-
-
-
-
 resource "azurerm_virtual_network_peering" "hub_vnet_to_jumphost_vnet" {
   name                      = "${local.hub_vnet_name}-to-${local.jumphost_vnet_name}"
   resource_group_name       = data.azurerm_resource_group.main_rg.name
